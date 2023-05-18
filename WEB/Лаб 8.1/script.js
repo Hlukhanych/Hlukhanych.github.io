67cd748b80f2f7426fa434c5121c8cd720de9870
Hlukhanych <hlukhanych.stanislav@student.uzhnu.edu.ua>
2023-05-18 11:48:46 +0300

    prototype

--- WEB/Лаб 8.1/script.js
+++ WEB/Лаб 8.1/script.js
@@ -6,11 +6,11 @@ function Accounting(code, fullName, position, salary, numberOfChildren, lengthOf
     this.salary = salary;
     this.numberOfChildren = numberOfChildren;
     this.lengthOfService = lengthOfService;
+}
 
-    this.getPerson = function (){
+Accounting.prototype.getPerson = function (){
         return `Code: ${this.code} \nFull name: ${this.fullName} \nPosition: ${this.position} \nSalary: ${this.salary} \nNumber of children: ${this.numberOfChildren} \nLength of service: ${this.lengthOfService}`;
     }
-}
 
 let persone1 = new Accounting(13, "Перманентний Владислав Борисович", "Керівник", 22000, 2, 4);
 let persone2 = new Accounting(14, "Гравірований Ярослав Вікторович", "Заступник", 19000, 0, 3);
@@ -24,11 +24,11 @@ function DAI(code, fullName, brand, carNumber, color){
     this.brand = brand;
     this.carNumber = carNumber;
     this.color = color;
+}
 
-    this.getPerson = function (){
+DAI.prototype.getPerson = function (){
         return `Code: ${this.code} \nFull name: ${this.fullName} \nBrand: ${this.brand} \nCar number: ${this.carNumber} \nColor: ${this.color}`;
     }
-}
 
 let persone3 = new DAI(13, "Перманентний Владислав Борисович", "BMW", "АО 2343 ВМ", "black");
 let persone4 = new DAI(14, "Гравірований Ярослав Вікторович", "Honda", "Ао 6547 ВР", "silver");
@@ -42,11 +42,11 @@ function Enterprise(code, enterpriseName, numberOfEmployees, industry, adress){
     this.numberOfEmployees = numberOfEmployees;
     this.industry = industry;
     this.adress = adress;
+}
 
-    this.getEnterprise = function (){
+Enterprise.prototype.getEnterprise = function (){
         return `Code: ${this.code} \nEnterprise name: ${this.enterpriseName} \nNumber of employees: ${this.numberOfEmployees} \nIndustry: ${this.industry} \nAderss: ${this.adress}`;
     }
-}
 
 let enterprise1 = new Enterprise(13, "Unet", 5, "internet", "Перечин, Центральна 22");
 let enterprise2 = new Enterprise(14, "Teviant", 18, "internet", "Ужгород");
